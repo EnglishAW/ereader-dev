0a1892dab2eaacbdb32595f41bc01a1f65e92928
HeavyByte <english.w.andrew@gmail.com>
2023-04-11 14:13:24 -0500

    counter button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+	import CounterButton from "../components/CounterButton.svelte";
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
+<CounterButton/>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
