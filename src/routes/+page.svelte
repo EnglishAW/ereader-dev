c679aa6a22378091b3c4cb1f807697c3ad85f47f
HeavyByte <english.w.andrew@gmail.com>
2023-04-11 14:25:43 -0500

    display for browser info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script>
 	import CounterButton from "../components/CounterButton.svelte";
+    import { browser } from '$app/environment'
+
+    $: currentUserAgent = browser && window.navigator.userAgent || "Not a browser" 
+    $: screenWidth = browser && window.innerWidth || "Not a browser" 
+    $: screenHeight = browser && window.innerHeight || "Not a browser" 
+
 
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <CounterButton/>
+<p>{currentUserAgent}</p>
+<p>{screenWidth} x {screenHeight}</p>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
