33abfdad97c95bd8d561309b22c69ce57ac1e336
HeavyByte <english.w.andrew@gmail.com>
2023-04-18 12:34:35 -0500

    test preact

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { h, Component, render } from 'preact';
+import htm from 'htm';
+
+// Initialize htm with Preact
+const html = htm.bind(h);
+
+function App (props) {
+  return html`<h1>Hello ${props.name}!</h1>`;
+}
+
+render(html`<${App} name="World" />`, document.body);
